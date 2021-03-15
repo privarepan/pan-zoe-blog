ea545b2279b00342e11fff1f7d2ac81e188c694a
潘章一 <panzhangyi@mythinkcar.com>
2021-03-15 15:42:44 +0800

    edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
         <div>
-            <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
+            <img class="mx-auto h-12 w-auto" src="{{asset('img/workflow-mark-indigo-600.svg')}}" alt="Workflow">
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 Pan Zoe 的博客
             </h2>
